28fe8b1830089744152e28471c94ba76431370d3
Kyle Townsend <KyleTTownsend@gmail.com>
2019-07-07 14:59:59 -0500

    dbcontext generation and initial changes

---
+++ TFTApp.Services/TFTApp.Data/Models/DAL/OriginBonus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TFTApp.Data.Models.DAL
+{
+    public partial class OriginBonus
+    {
+        public int BonusId { get; set; }
+        public short Needed { get; set; }
+        public string Effect { get; set; }
+    }
+}
